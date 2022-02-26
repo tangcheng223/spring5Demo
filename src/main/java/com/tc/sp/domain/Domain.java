86071ba3760aae65790d08f5956edd3087388057
唐诚 <6566313+tang_cheng223@user.noreply.gitee.com>
2022-02-26 21:58:57 +0800

    first

--- src/main/java/com/tc/sp/domain/Domain.java
+++ src/main/java/com/tc/sp/domain/Domain.java
@@ -1,9 +1,40 @@
 package com.tc.sp.domain;
 
+import java.util.List;
+import java.util.Map;
+
 public class Domain {
     private int id;
 
     private String title;
+
+    private List<String> list;
+
+    private Map<Integer, String> map;
+
+    public List<String> getList() {
+        return list;
+    }
+
+    public void setList(List<String> list) {
+        this.list = list;
+    }
+
+    public Map<Integer, String> getMap() {
+        return map;
+    }
+
+    public void setMap(Map<Integer, String> map) {
+        this.map = map;
+    }
+
+    public Domain() {
+    }
+
+    public Domain(String title) {
+        System.out.println("constructor set");
+        this.title = title;
+    }
 
     public int getId() {
         return id;
@@ -18,6 +49,7 @@ public class Domain {
     }
 
     public void setTitle(String title) {
+        System.out.println("setTitle set");
         this.title = title;
     }
 }
--- src/main/java/com/tc/test.java
+++ src/main/java/com/tc/test.java
@@ -5,14 +5,28 @@ import com.tc.sp.domain.DomainOrder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import javax.naming.Context;
+
 public class test {
     public static void main(String[] args) {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
 
+       // extracted(applicationContext);v
+        testInjectCollection(applicationContext);
+    }
+
+    private static void extracted(ApplicationContext applicationContext) {
         Domain domain = (Domain) applicationContext.getBean("domain");
         DomainOrder domainOrder = (DomainOrder) applicationContext.getBean("domainOrder");
         System.out.println(domain.getTitle());
         System.out.println(domainOrder.getOderNo());
         System.out.println(domainOrder.getDomain().getTitle());
+    }
+
+    private static void testInjectCollection(ApplicationContext applicationContext)
+    {
+        Domain domain = (Domain) applicationContext.getBean("domain");
+        System.out.println(domain.getList());
+        System.out.println(domain.getMap());
     }
 }
